a454a767edd1768f3be2f1798560866de620eae4
haqe.s2 <haqe.s2@P-0G-0B005-0398>
2022-03-31 10:56:28 +0200

    Projektdateien hinzufügen.

---
+++ Methoden 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    public static void Main ()
+
+
+        {
+        Console.WriteLine("Geben Sie bitte ein Wort ein ");
+        string n = (Console.ReadLine());
+        Console.WriteLine("Geben Sie bitte  ein Zahl ein  ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        int anzahl = 0;
+        
+        anzahl = ausgabeTextMalX(n,m);
+        
+
+        Console.Write("\nAnzahl der Buchstaben: {0}", anzahl);
+
+        }
+    static int  ausgabeTextMalX (string name,int zahl)
+    {
+        for(int i =0; i <zahl;i++)
+        {
+
+            Console.WriteLine(name);
+
+
+        }
+
+        
+        return zahl * name.Length;
+    }
+    
+
+}
